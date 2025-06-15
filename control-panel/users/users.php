f80713ffa39e456a304505e545f338609e271314
Gerum-Berhanu <gerumberhanu0@gmail.com>
2025-06-15 11:19:35 +0300

    Listing of users

--- control-panel/users/users.php
+++ control-panel/users/users.php
@@ -1,3 +1,13 @@
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "/includes/auth.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/scripts/classes.php";
+
+$db = new Database();
+
+$rows = $db->execute_sql("SELECT full_name, email FROM users", [], true);
+
+?>
+
 <!doctype html>
 <!-- 
 * Bootstrap Simple Admin Template
@@ -86,97 +96,22 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>Philip Chaney</td>
-                                        <td>philip.chaney@gmail.com</td>
-                                        <td>Manager</td>
-                                        <td>Admin</td>
-                                        <td>Active</td>
-                                        <td class="text-end">
-                                            <a href="" class="btn btn-outline-info btn-rounded"><i
-                                                    class="fas fa-pen"></i></a>
-                                            <a href="" class="btn btn-outline-danger btn-rounded"><i
-                                                    class="fas fa-trash"></i></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Doris Greene</td>
-                                        <td>ms.greene@outlook.com</td>
-                                        <td>Writer</td>
-                                        <td>Staff</td>
-                                        <td>Active</td>
-                                        <td class="text-end">
-                                            <a href="" class="btn btn-outline-info btn-rounded"><i
-                                                    class="fas fa-pen"></i></a>
-                                            <a href="" class="btn btn-outline-danger btn-rounded"><i
-                                                    class="fas fa-trash"></i></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Mason Porter</td>
-                                        <td>mason_porter@gmail.com</td>
-                                        <td>Contributor</td>
-                                        <td>Staff</td>
-                                        <td>Active</td>
-                                        <td class="text-end">
-                                            <a href="" class="btn btn-outline-info btn-rounded"><i
-                                                    class="fas fa-pen"></i></a>
-                                            <a href="" class="btn btn-outline-danger btn-rounded"><i
-                                                    class="fas fa-trash"></i></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Minerva Hooper</td>
-                                        <td>minerva.hooper@gmail.com</td>
-                                        <td>Administrator</td>
-                                        <td>Admin</td>
-                                        <td>Disabled</td>
-                                        <td class="text-end">
-                                            <a href="" class="btn btn-outline-info btn-rounded"><i
-                                                    class="fas fa-pen"></i></a>
-                                            <a href="" class="btn btn-outline-danger btn-rounded"><i
-                                                    class="fas fa-trash"></i></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Jessie Williams</td>
-                                        <td>jessie@gmail.com</td>
-                                        <td>Administrator</td>
-                                        <td>Admin</td>
-                                        <td>Active</td>
-                                        <td class="text-end">
-                                            <a href="" class="btn btn-outline-info btn-rounded"><i
-                                                    class="fas fa-pen"></i></a>
-                                            <a href="" class="btn btn-outline-danger btn-rounded"><i
-                                                    class="fas fa-trash"></i></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Peter Benhams</td>
-                                        <td>pette@gmail.com</td>
-                                        <td>Editor</td>
-                                        <td>Staff</td>
-                                        <td>Active</td>
-                                        <td class="text-end">
-                                            <a href="" class="btn btn-outline-info btn-rounded"><i
-                                                    class="fas fa-pen"></i></a>
-                                            <a href="" class="btn btn-outline-danger btn-rounded"><i
-                                                    class="fas fa-trash"></i></a>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Jose Rodriguez</td>
-                                        <td>jose.rodz@gmail.com</td>
-                                        <td>Author</td>
-                                        <td>Staff</td>
-                                        <td>Active</td>
-                                        <td class="text-end">
-                                            <a href="" class="btn btn-outline-info btn-rounded"><i
-                                                    class="fas fa-pen"></i></a>
-                                            <a href="" class="btn btn-outline-danger btn-rounded"><i
-                                                    class="fas fa-trash"></i></a>
-                                        </td>
-                                    </tr>
+                                    <?php
+                                    foreach ($rows as $row) {
+                                        echo 
+                                        '<tr>
+                                            <td>' . htmlspecialchars($row["full_name"]) . '</td>
+                                            <td>' . htmlspecialchars($row["email"]) . '</td>
+                                            <td>Manager</td>
+                                            <td>Admin</td>
+                                            <td>Active</td>
+                                            <td class="text-end">
+                                                <a href="" class="btn btn-outline-info btn-rounded"><i class="fas fa-pen"></i></a>
+                                                <a href="" class="btn btn-outline-danger btn-rounded"><i class="fas fa-trash"></i></a>
+                                            </td>
+                                        </tr>';
+                                    }
+                                    ?>
                                 </tbody>
                             </table>
                         </div>
